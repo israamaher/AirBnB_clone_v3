b51d31b2acb53e8ab47a23c1f4f3d9b4f5b35943
israa maher <esraamaher770@gmail.com>
2024-03-31 19:48:44 -0700

    update api1

---
+++ api/v1/views/__init__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Blueprint for API  
+    larger applications it’s recommended to divide them into smaller groups 
+    where each group is implemented with the help of a blueprint
+    more info https://flask.palletsprojects.com/en/1.1.x/blueprints/#blueprints
+"""
+from flask import Blueprint
+
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+from api.v1.views.index import *
+from api.v1.views.cities import *
+from api.v1.views.states import *
+from api.v1.views.users import *
+from api.v1.views.amenities import *
+from api.v1.views.places_reviews import *
+from api.v1.views.places import *
+from api.v1.views.places_amenities import *
